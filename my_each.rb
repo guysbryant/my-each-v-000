5036feb9957e3cae872bfb7da6a634fcd61bb2a9
Guy Bryant <guy.s.bryant@gmail.com>
2018-09-11 02:32:20 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,7 @@ def my_each(collection)  # put argument(s) here
   if block_given?
     i = 0
     while i < collection.length
-       collection[i]
+      yield collection[i]
        i += 1
     end
   end
